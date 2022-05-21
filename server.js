15209573876e55fb92a9994d8a5f0fb7a8aeec83
Rounak Singh <rounaksingh1694@gmail.com>
2022-05-21 21:22:58 +0530

    API working

--- server.js
+++ server.js
@@ -45,7 +45,12 @@ mongoose.connect(
 
 const app = express();
 
-// app.use(cors());
+const corsOptions = {
+	origin: "http://localhost:3000",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const port = process.env.PORT ? process.env.PORT : 8000;
